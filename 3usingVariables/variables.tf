6d7abc31fa0c98c0470b49753cab8a29ac4c20ea
partha biswas <biswas.partha1@gmail.com>
2024-01-26 18:45:53 +0530

    using variables

---
+++ 3usingVariables/variables.tf
@@ -0,0 +1,15 @@
+variable "filename" {
+    default = "sample.txt"
+}
+variable "content" {
+    default = "I love India"
+}
+variable "prefix" {
+    default = "Mrs"
+}
+variable "separator" {
+    default = "."
+}
+variable "length" {
+    default = "1"
+}
